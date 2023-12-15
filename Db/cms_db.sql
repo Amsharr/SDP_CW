aa17fe5d8b842ad985884f0d1f565ef45d4a64c7
Amshar Mushtaq <amshar.m@medoment.com>
2023-12-15 23:18:16 +0530

    Updated the SQL to add tables and fields.

--- Db/cms_db.sql
+++ Db/cms_db.sql
@@ -1,60 +1,96 @@
--- Create Institutions Table
+-- Create Table: institutions
 CREATE TABLE institutions (
     institutionId INT PRIMARY KEY,
-    type VARCHAR(50) NOT NULL,
-    name VARCHAR(255) NOT NULL
+    type VARCHAR(255) NOT NULL
 );
 
--- Create Complainers Table
+-- Create Table: complainers
 CREATE TABLE complainers (
-    userId INT PRIMARY KEY AUTO_INCREMENT,
+    userId INT AUTO_INCREMENT PRIMARY KEY,
     firstName VARCHAR(255) NOT NULL,
     lastName VARCHAR(255) NOT NULL,
-    contactNo VARCHAR(20),
+    address VARCHAR(255) NOT NULL,
+    contactNo VARCHAR(15) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    username VARCHAR(50) NOT NULL,
+    username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
 
--- Create Complains Table
-CREATE TABLE complains (
-    complainId INT PRIMARY KEY AUTO_INCREMENT,
-    userId INT NOT NULL,
-    institutionId INT NOT NULL,
-    Description TEXT NOT NULL,
-    status VARCHAR(50) NOT NULL,
-    dateTime DATETIME NOT NULL,
+-- Create Table: complaints
+CREATE TABLE complaints (
+    complaintId INT AUTO_INCREMENT PRIMARY KEY,
+    userId INT,
+    complainerFirstName VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    status VARCHAR(255) DEFAULT 'open',
+    date DATE NOT NULL,
     FOREIGN KEY (userId) REFERENCES complainers(userId),
-    FOREIGN KEY (institutionId) REFERENCES institutions(institutionId)
+    CONSTRAINT FK_complaints_complainers
+        FOREIGN KEY (userId)
+        REFERENCES complainers(userId)
 );
 
--- Create AreaOfficers Table
+-- Create Table: areaOfficers
 CREATE TABLE areaOfficers (
-    officerId INT PRIMARY KEY AUTO_INCREMENT,
+    officerId INT AUTO_INCREMENT PRIMARY KEY,
+    instituitionId INT,
     firstName VARCHAR(255) NOT NULL,
     lastName VARCHAR(255) NOT NULL,
-    role VARCHAR(50) NOT NULL,
-    username VARCHAR(50) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    instituitionId INT NOT NULL,
+    contactNo VARCHAR(15) NOT NULL,
+    position VARCHAR(255) NOT NULL,
     FOREIGN KEY (instituitionId) REFERENCES institutions(institutionId)
 );
 
--- Create InvestigationOfficers Table
+-- Create Table: investigationOfficers
 CREATE TABLE investigationOfficers (
-    officerId INT PRIMARY KEY AUTO_INCREMENT,
+    officerId INT AUTO_INCREMENT PRIMARY KEY,
     firstName VARCHAR(255) NOT NULL,
     lastName VARCHAR(255) NOT NULL,
-    role VARCHAR(50) NOT NULL,
-    username VARCHAR(50) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    instituitionId INT NOT NULL,
+    contactNo VARCHAR(15) NOT NULL,
+    position VARCHAR(255) NOT NULL,
+    instituitionId INT,
     FOREIGN KEY (instituitionId) REFERENCES institutions(institutionId)
 );
 
--- Create SystemAdmin Table
+-- Create Table: systemAdmin
 CREATE TABLE systemAdmin (
-    systemAdminId INT PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(50) NOT NULL,
+    systemAdminId INT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE currentComplaints (
+    complaintId INT,
+    officerId INT,
+    description TEXT NOT NULL,
+    date DATE NOT NULL,
+    status VARCHAR(255) DEFAULT 'open',
+    investigatorRemarks TEXT,
+    FOREIGN KEY (complaintId) REFERENCES complaints(complaintId),
+    FOREIGN KEY (officerId) REFERENCES investigationOfficers(officerId)
+);
+
+-- Create Table: complaintRemark
+CREATE TABLE complaintRemark (
+    Id INT AUTO_INCREMENT PRIMARY KEY,
+    complainNumber INT,
+    status VARCHAR(255) NOT NULL,
+    remark TEXT NOT NULL,
+    remarkDate DATE NOT NULL
+);
+
+SELECT
+    cc.complaintId,
+    cc.officerId,
+    c.description AS complaintDescription,
+    c.status AS complaintStatus,
+    c.date AS complaintDate,
+    cc.date AS currentComplaintDate,
+    cc.status AS currentComplaintStatus,
+    cc.investigatorRemarks
+FROM
+    currentComplaints cc
+JOIN
+    complaints c ON cc.complaintId = c.complaintId;
+
