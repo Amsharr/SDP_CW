551f8ff5195b82cf6ad3bc805e24789844255087
Amshar Mushtaq <amshar.m@medoment.com>
2023-12-18 23:27:13 +0530

    issue with folder path

--- System admin/AreaOfficers/editOfficer.php
+++ System admin/AreaOfficers/editOfficer.php
@@ -47,7 +47,7 @@
       <a class="navbar-brand" href="#">Complaint Management System</a>      
       <div class="ml-auto">
         <div class="dropdown mt-3" style="margin-bottom: 10px;">                        
-            <a class="btn btn-warning" href="./login.php" role="button"><i class="fa-solid fa-arrow-right"></i> <span class="ms-1 d-none d-sm-inline">Logout</span></a>
+            <a class="btn btn-warning" href="../login.php" role="button"><i class="fa-solid fa-arrow-right"></i> <span class="ms-1 d-none d-sm-inline">Logout</span></a>
         </div> 
       </div>
     </div>
